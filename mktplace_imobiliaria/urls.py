bedc39e5246b03e1b94af54582776dcaa9280257
Dog <jvcbatist4@outlook.com>
2025-01-29 14:29:16 -0300

    Setup Token JWT and routes in urls.py

--- mktplace_imobiliaria/urls.py
+++ mktplace_imobiliaria/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshView )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obatin_pair'), # p/ realizar o login
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'), # refresh do tk
+    
 ]
