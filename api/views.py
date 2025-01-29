c74b0edb8873f11ddd9b6070f80a7263526b72ad
Dog <jvcbatist4@outlook.com>
2025-01-29 14:40:46 -0300

    Added protection JWT to views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 from rest_framework import viewsets
 from .models import Imobiliaria, Imovel, Imagem, PacoteAnuncio, Contrato
 from .serializers import ImobiliariaSerializer, ImovelSerializer, ImagemSerializer, PacoteAnuncioSerializer, ContratoSerializer
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 class ImobiliariaViewSet(viewsets.ModelViewSet):
     queryset = Imobiliaria.objects.all()
     serializer_class = ImobiliariaSerializer
@@ -14,10 +19,14 @@ class ImagemViewSet(viewsets.ModelViewSet):
     queryset = Imagem.objects.all()
     serializer_class = ImagemSerializer
 
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 class PacoteAnuncioViewSet(viewsets.ModelViewSet):
     queryset = PacoteAnuncio.objects.all()
     serializer_class = PacoteAnuncioSerializer
-
+    
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 class ContratoViewSet(viewsets.ModelViewSet):
     queryset = Contrato.objects.all()
     serializer_class = ContratoSerializer
