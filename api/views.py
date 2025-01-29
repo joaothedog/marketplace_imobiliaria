f5e09d1cc6c16462364b5d6470cf80b95eb8beb1
Dog <jvcbatist4@outlook.com>
2025-01-29 14:16:48 -0300

    Added views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Imobiliaria, Imovel, Imagem, PacoteAnuncio, Contrato
+from .serializers import ImobiliariaSerializer, ImovelSerializer, ImagemSerializer, PacoteAnuncioSerializer, ContratoSerializer
 
-# Create your views here.
+class ImobiliariaViewSet(viewsets.ModelViewSet):
+    queryset = Imobiliaria.objects.all()
+    serializer_class = ImobiliariaSerializer
+
+class ImovelViewSet(viewsets.ModelViewSet):
+    queryset = Imovel.objects.all()
+    serializer_class = ImovelSerializer
+
+class ImagemViewSet(viewsets.ModelViewSet):
+    queryset = Imagem.objects.all()
+    serializer_class = ImagemSerializer
+
+class PacoteAnuncioViewSet(viewsets.ModelViewSet):
+    queryset = PacoteAnuncio.objects.all()
+    serializer_class = PacoteAnuncioSerializer
+
+class ContratoViewSet(viewsets.ModelViewSet):
+    queryset = Contrato.objects.all()
+    serializer_class = ContratoSerializer
