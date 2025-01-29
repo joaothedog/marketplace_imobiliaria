e037966686d01c0b1d6ede49c059e88c50b2c3fb
Dog <jvcbatist4@outlook.com>
2025-01-29 15:26:53 -0300

    Serializer to register a user

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,32 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from .models import Imobiliaria, Imovel, Imagem, PacoteAnuncio, Contrato
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'email': {'required': True},
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "As senhas não coincidem."})
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 class ImobiliariaSerializer(serializers.ModelSerializer):
     class Meta:
